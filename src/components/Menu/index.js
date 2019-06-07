184a4e392bc931787fdc5bbdb989b0f41600a255
JaburMacbookPro <jaburmail@gmail.com>
2019-06-07 16:12:24 -0300

    Implementacao da animacao

---
+++ src/components/Menu/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import QRCode from 'react-native-qrcode';
+
+import {
+  Container, Code, Nav, NavItem, NavText, SignOutButtom, SignOutButtomText,
+} from './styles';
+
+export default function Menu({ translateY }) {
+  return (
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+      }),
+    }}
+    >
+      <Code>
+        <QRCode
+          value="https://github.com/JaburBr/ReactNative-Nubank"
+          size={80}
+          fgColor="#FFF"
+          bgColor="#8B10AE"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartao</NavText>
+        </NavItem>
+
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configuracoes do App</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButtom onPress={() => { }}>
+        <SignOutButtomText>Sair do APP</SignOutButtomText>
+      </SignOutButtom>
+
+    </Container>
+  );
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,97 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
+import {
+  Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation,
+} from './styles';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 export default function Main() {
+  let offSet = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offSet += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offSet);
+        translateY.setOffset(0);
+        offSet = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offSet = opened ? 380 : 0;
+        translateY.setOffset(offSet);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
     <Container>
       <Header />
-      <Tabs />
+
+      <Content>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 123.456,78</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferencia de 100,00 recebida de Lucas Jabur as 18:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
+      </Content>
+
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
